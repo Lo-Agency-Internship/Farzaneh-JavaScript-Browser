c182085470594680c8d69149509bfb1a4c6878c3
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-05-16 17:33:39 +0430

    first

---
+++ JS/javascript.js
@@ -0,0 +1,14 @@
+// Hex Change Background Color
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
+}
+
+genNew.addEventListener("click", setBg);
+setBg();
+
+
+// resources:https://css-tricks.com/snippets/javascript/random-hex-color/
+
