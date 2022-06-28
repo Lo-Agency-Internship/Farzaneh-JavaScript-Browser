fa76de5b7b74de738130575e23ab78d1226c52a0
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-06-28 13:26:41 +0430

    filter by click and search

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@ const liItem = document.querySelectorAll('ul li');
 const imgItem = document.querySelectorAll('.Objects img');
 
 liItem.forEach(li => {
-    li.addEventListener ( "click" ,function () {
+    li.addEventListener("click", function () {
         //active
         liItem.forEach(li => {
             li.className = "";
@@ -11,10 +11,8 @@ liItem.forEach(li => {
 
         //Filter
         const value = li.textContent;
-        console.log(value)
 
-        if (value.toLowerCase().trim()=== "all") {
-            console.log("value1")
+        if (value.toLowerCase().trim() === "all") {
             imgItem.forEach(img => {
                 img.style.display = 'block';
             })
@@ -24,28 +22,26 @@ liItem.forEach(li => {
             img.style.display = 'none';
         })
         const selectedImg = document.querySelectorAll(`${"." + value.toLowerCase()}`)
-        console.log(selectedImg)
         selectedImg.forEach(img => {
             img.style.display = 'block';
         })
     })
 });
 // =================searchBar=============
-
-const allItems = document.getElementsByClassName("item");
-
-function searchBar() {
-  let search = document.getElementById("searchBar");
-  search = search.toLowerCase().trim();
-  console.log("search")
-  let allItems = document.getElementsByClassName('Items');
-    for (i = 0; i < allItems.length; i++) { 
-        if (!allItems[i].innerHTML.toLowerCase().includes(search)) {
-            allItems[i].style.display="none";
-        }
-        else {
-            allItems[i].style.display="list-item";                 
-        }
+function searchBar(){
+    const search = document.getElementById("searchBarText").value;
+    const images = document.querySelectorAll("img");
+    if (search === "all") {
+        images.forEach(img => {
+            img.style.display = 'block';
+        })
+        return 
     }
-}
-
+    imgItem.forEach(img => {
+        img.style.display = 'none';
+    })
+    const selectedImg = document.querySelectorAll(`${"." + search.toLowerCase()}`)
+    selectedImg.forEach(img => {
+        img.style.display = 'block';
+    })
+} 
